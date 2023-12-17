6307a433a12c5ad035fc5b6cfbb7d7a389a21697
MR-VSR <vikasraivsr1234@gmail.com>
2023-12-17 18:39:40 +0530

    conversion of datatypes

---
+++ basics/conversions.js
@@ -0,0 +1,50 @@
+//In this file we will learn about conversion from one data type to another and its working in js
+
+// Conversion to Number
+
+// let score = "33"
+// console.log(typeof score)
+
+// let num = Number(score)
+/*Converting any other data types to number.
+NOTE => if the value being converted can not be represented in number format then we'll be getting a special number type which is NaN which means not a number.
+*/
+
+/* Other conversion include
+true => 1 false => 0
+null => 0
+undefined => NaN
+"12" => 12
+"abc" => NaN
+*/
+
+// console.log(typeof num)//output => number   and value stored in num will be 33
+
+// console.log(typeof NaN)
+// console.log(Number(null))
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number(undefined))
+// console.log(Number("abc"))
+
+// Conversion to Boolean
+
+// let n = 1
+// let bool = Boolean(n) //Output => true
+
+// console.log(Boolean(1))//Output =>true
+// console.log(Boolean(0))//Output =>false
+// console.log(Boolean(""))//Output =>false
+// console.log(Boolean("Hello"))//Output =>true
+
+//Conversion to String
+
+// let someNum = 33
+// let str = String(someNum)
+// console.log(typeof str)
+
+
+
+
+
+
